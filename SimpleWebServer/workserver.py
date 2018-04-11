77b93eeee285b62f50c13d162823aa2f3a7ed74d
Ziv Rafalovich <zivr@microsoft.com>
2018-04-10 23:46:39 -0700

    print hostname

--- SimpleWebServer/workserver.py
+++ SimpleWebServer/workserver.py
@@ -33,7 +33,7 @@ worker_thread.start()
 
 def writebody():
     body = '<html><head><title>Work interface - build</title></head>'
-    body += '<body><h2>Worker interface on ' + hostname + '</h2><ul><h3>'
+    body += '<body><h2>Worker interface on ' + socket.gethostname() + '</h2><ul><h3>'
 
     if keepworking == False:
         body += '<br/>Worker thread is not running. <a href="./do_work">Start work</a><br/>'
